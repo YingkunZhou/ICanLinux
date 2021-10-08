00c05c0d835a750dedb34f2f67b634fdbedd76a8
YingkunZhou <zhouyingkun19@163.com>
2021-10-08 16:46:54 +0800

    add apps

--- riscv-rootfs/apps/hello/hello.c
+++ riscv-rootfs/apps/hello/hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+// #include <sys/reboot.h>
+// #include <unistd.h>
+
 int main() {
 	printf("Hello, RISC-V World!\n");
-	printf("hanging\n");
-	while(1);
+
+  /* reboot(RB_POWER_OFF);
+   * for (;;)
+   *   sleep(1);
+   * return 0; */
+
+  // printf("hanging\n");
+  // while(1);
+  return 0;
 }
